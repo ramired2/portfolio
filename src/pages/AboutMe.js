8a409b5123ca18ed5830512ac182c09b5721a1c0
Daisy <ramired2@oregonstate.edu>
2023-06-14 22:09:53 -0700

    fixed formatting w facts/interests

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -13,7 +13,7 @@ const AboutMe = () => {
       </div>
       {/* </div> */}
       <div className='section'>
-    {/* <div className=''> */}
+    <div className='indivSect'>
       <h6 className='subsubtopic text'>My Interests</h6>
       <div className='description line text'>
         <p >i first became interested in CS in my 
@@ -25,22 +25,30 @@ const AboutMe = () => {
             was insightful and introduced me into something that I had only ever 
             associated with hacking like i'd seen on TV or in movies.  
         </p>
-        {/* </div> */}
-        <p > I had become so interested in CS that for 
+        
+        <p className='text'> I had become so interested in CS that for 
         my sophemore project, akin to a senior project, I taught myself how to code on 
         CodeAcademy. Looking back on that project and seeing the work I am and have been 
         able to produce since then is amazing! As someone who loves learning 
         new things, to see how I've progressed so far, I can't wait 
         to continue to become better through future projects!</p>
-        </div>
-        {/* <div className=''> */}
+        </div></div>
+        <div className='indivSect indivGroup'>
         <h6 className='subsubtopic  text interestAlign'>Quick Facts</h6>
       <ul className='listAlign line'>
         <li className='text description listAlign firstListItem'>i transfered to Oregon State from Pacific University in 2020</li>
         <li className='text description listAlign'>i first wanted to be a CS major in ~2015</li>
         <li className='text description listAlign'>i love learning new things whether it be for profesional or personal purposes</li>
+        <li className='text description listAlign'>i love learning new things whether it be for profesional or personal purposes</li>
       </ul>
-      {/* </div> */}
+        {/* <h6 className='subsubtopic  text interestAlign'>Fun Facts</h6>
+      <ul className='listAlign line'>
+        <li className='text description listAlign firstListItem'>i transfered to Oregon State from Pacific University in 2020</li>
+        <li className='text description listAlign'>i first wanted to be a CS major in ~2015</li>
+        <li className='text description listAlign'>i love learning new things whether it be for profesional or personal purposes</li>
+        <li className='text description listAlign'>i love learning new things whether it be for profesional or personal purposes</li>
+      </ul> */}
+      </div>
       </div>
         
       <div>
